4c3778008547e22584171f0ace10c0a9143a1cd3
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-25 10:46:14 +0900

    속성(Property) 실습

--- ClassAdv/Program.cs
+++ ClassAdv/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,19 @@ namespace ClassAdv
             Product prouduct1 = new Product("과자", 1000);
             Product prouduct2 = new Product("아이스크림", 1000);
             Product prouduct3 = new Product("케이크", 1000);
+
+            // 속성(Property) 실습
+            Box box = new Box(10, 10);
+            box.Width = 100;
+            box.Height = 100;
+            box.GetArea();
+            try
+            {
+                box.Width = -10; // 잘못된 값이 들어간 경우
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
