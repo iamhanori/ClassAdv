c6bfb6b567a9350178a1b8292b10f83c985f7fb4
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-25 10:39:31 +0900

    소멸자 실습

--- ClassAdv/Program.cs
+++ ClassAdv/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,10 @@ namespace ClassAdv
     {
         static void Main(string[] args)
         {
+            // 소멸자 예시
+            Product prouduct1 = new Product("과자", 1000);
+            Product prouduct2 = new Product("아이스크림", 1000);
+            Product prouduct3 = new Product("케이크", 1000);
         }
     }
 }
